5c8e94be7dd8b71ff774ddfffa9824f921d5810f
zahdr <f.wischendorff@posteo.de>
2023-06-19 02:44:56 +0200

    added slashcommands /lpi v and /lpi reset (#11)

--- classes/slashCommands.lua
+++ classes/slashCommands.lua
@@ -2,6 +2,12 @@ local _, LPI = ...;
 
 LPI.Ace:RegisterChatCommand("lpi", "SlashCommand");
 
-function LPI.Ace:SlashCommand()
-	lootImportOverview:draw()
+function LPI.Ace:SlashCommand(msg)
+	if msg == "reset" then
+		db:resetLootCouncilData()
+	elseif msg == "v" then
+		LPI:infoMessage(LPI.name .. " " .. LPI.version)
+	else
+		lootImportOverview:draw()
+	end
 end
